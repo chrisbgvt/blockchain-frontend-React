d0439028c4160ee942c7a92d542916bf31582a3f
chrisbgvt <kristiqn.georgiev.bg@gmail.com>
2023-06-26 23:33:30 +0300

    refactor

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -27,49 +27,49 @@ export function useElectionContract(connectedWallets: any[]): {
     loading: boolean;
     electionId: number;
     // elections: string;
-  } {
+} {
     const [electionId, setElectionId] = useState<number>(0);
     const [elections, setElections] = useState<string[]>([]);
     const [contracts, setContracts] = useState<(ethers.Contract | null)[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
-      const fetchContracts = async () => {
-        try {
-          const contract = getContract(connectedWallets);
-          const electionId = await contract.electionId();
-    
-          const electionsPromises = Array.from({ length: electionId }, (_, index) =>
-            contract.elections(index)
-          );
-          const elections = await Promise.all(electionsPromises);
-    
-          if (connectedWallets.length > 0) {
-            const injectedProvider = connectedWallets[0].provider;
-            const provider = new ethers.providers.Web3Provider(injectedProvider);
-            const signer = provider.getSigner();
-            
-            const contracts = elections.map(x =>
-              new ethers.Contract(x, ELECTION_ABI, signer)
-            );
+        const fetchContracts = async () => {
+            try {
+                const contract = getContract(connectedWallets);
+                const electionId = await contract.electionId();
 
-            setContracts(contracts);
-          }
-        } catch (error) {
-          console.error('Error fetching contracts:', error);
-        }
-      };
-    
-      fetchContracts().then(() => setLoading(false));
+                const electionsPromises = Array.from({ length: electionId }, (_, index) =>
+                    contract.elections(index)
+                );
+                const elections = await Promise.all(electionsPromises);
+
+                if (connectedWallets.length > 0) {
+                    const injectedProvider = connectedWallets[0].provider;
+                    const provider = new ethers.providers.Web3Provider(injectedProvider);
+                    const signer = provider.getSigner();
+
+                    const contracts = elections.map(x =>
+                        new ethers.Contract(x, ELECTION_ABI, signer)
+                    );
+
+                    setContracts(contracts);
+                }
+            } catch (error) {
+                console.error('Error fetching contracts:', error);
+            }
+        };
+
+        fetchContracts().then(() => setLoading(false));
     }, [connectedWallets]);
-  
+
     return { contracts, loading, electionId };
 }
 
 export function handleError(error: any) {
-  const errorMessage = (error as Error).toString();
-  const regex = /reverted with reason string '(.*?)'/;
-  const match = errorMessage.match(regex);
-  const extractedMessage = match ? match[1] : errorMessage;
-  return extractedMessage;
+    const errorMessage = (error as Error).toString();
+    const regex = /reverted with reason string '(.*?)'/;
+    const match = errorMessage.match(regex);
+    const extractedMessage = match ? match[1] : errorMessage;
+    return extractedMessage;
 }
